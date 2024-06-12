27cf2d90843ca501cbfdf136fb5fdc18c029a1e2
John J. Lawal <jophilip79@gmail.com>
2024-06-12 21:15:01 +0100

    Prepare to deploy

--- website/views.py
+++ website/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
+
 
 # Create your views here.
+def landing(request):
+    return render(request, 'website/pages/web.html')
+
